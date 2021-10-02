1e42819cbb19f0015e978b8c4e969340f860994c
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-10-02 17:00:54 -0300

    cree clase MarcaNegocio y su metodo Listar.

---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    class MarcaNegocio
+    {
+        public List<Marca> listar(){
+            List<Marca> aux = new List<Marca>();
+            AccesoDatos dato = new AccesoDatos();
+            try
+            {
+                dato.setearConsulta("SELECT MARCA.Id, MARCA.Descripcion FROM MARCA");
+                dato.ejecutarLectura();
+                while (dato.Lector.Read())
+                {
+                    Marca extra = new Marca();
+                    extra.idMarca = (int)dato.Lector["Id"];
+                    extra.descripcionMarca = (string)dato.Lector["Descripcion"];
+                    aux.Add(extra);                    
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            dato.cerrarConexion();
+            return aux;
+        }
+    }
+}
